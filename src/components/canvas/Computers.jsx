c752cfb70f9b68c042376df179a6305be66a5550
Anees-khan <aneeskhan01001@gmail.com>
2026-01-02 20:06:23 +0500

    Adjusted the 3d Model and tweaked the lighting

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,7 +9,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={1.15} groundColor='black' />
+      <hemisphereLight intensity={1.85} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.5 : 0.75}
+        position={isMobile ? [0, -2.2, -1.3] : [0, -3.45, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
